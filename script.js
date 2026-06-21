adf63ef9cb40323f277917b73a79b7610076f716
Ivan <hotredperchik@gmail.com>
2026-06-21 19:57:11 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ document.getElementById('subscribeForm').addEventListener('submit', async functi
     const city = document.getElementById('city').value.trim();
     const message = document.getElementById('message');
 
-    // Простая валидация
     if (!email || !city) {
         message.className = 'message error';
         message.textContent = 'Заполните оба поля!';
         return;
     }
 
-    // Здесь ТВОЙ URL-адрес вебхука из n8n
-    // Как получишь — вставь его сюда
     const WEBHOOK_URL = 'https://girsha.app.n8n.cloud/webhook/08eab8dd-d7f1-4705-8d87-359974e4c9d3';
 
     try {
@@ -27,15 +24,15 @@ document.getElementById('subscribeForm').addEventListener('submit', async functi
 
         if (response.ok) {
             message.className = 'message success';
-            message.textContent = '✅ Подписка оформлена! Проверьте вашу почту.';
+            message.textContent = 'Подписка оформлена! Проверьте вашу почту.';
             document.getElementById('subscribeForm').reset();
         } else {
             message.className = 'message error';
-            message.textContent = '❌ Ошибка при отправке. Попробуйте позже.';
+            message.textContent = 'Ошибка при отправке. Попробуйте позже.';
         }
     } catch (error) {
         message.className = 'message error';
-        message.textContent = '❌ Ошибка соединения. Проверьте интернет.';
+        message.textContent = 'Ошибка соединения. Проверьте интернет.';
         console.error('Ошибка:', error);
     }
 });
